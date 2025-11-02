3b87016ed2c24553fcb33b088ebde2715f79a85b
Hyunjong <hj6561@naver.com>
2025-11-02 15:52:58 +0900

    개발 환경 세팅

---
+++ src/components/Providers.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { Provider } from "jotai";
+import { ReactNode } from "react";
+
+const Providers = ({ children }: { children: ReactNode }) => {
+  return <Provider>{children}</Provider>;
+};
+
+export default Providers;
